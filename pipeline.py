65fd7c590ff33573f3cc1748c1bc08729cedd60b
Arkiver2 <arkiver@protonmail.com>
2019-11-02 22:14:36 +0100

    Set useragent to Archive Team; Update version to 20191102.04.

--- pipeline.py
+++ pipeline.py
@@ -56,8 +56,8 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20191102.03'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+VERSION = '20191102.04'
+USER_AGENT = 'Archive Team'
 TRACKER_ID = 'radio24syv'
 TRACKER_HOST = 'tracker.archiveteam.org'
 
